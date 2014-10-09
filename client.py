414e96390202166b9d68b07da53b4cc83d885ee3
Gregorio Robles <grex@computer.org>
2014-10-09 09:59:34 +0200

    Código de inicio de la práctica 4

---
+++ client.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python
+# -*- coding: iso-8859-15 -*-
+"""
+Programa cliente que abre un socket a un servidor
+"""
+
+import socket
+
+# Cliente UDP simple.
+
+# Dirección IP del servidor.
+SERVER = 'localhost'
+PORT = 6001
+
+LINE = '¡Hola mundo!'
+
+my_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+my_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+my_socket.connect((SERVER, PORT))
+print "Enviando: " + LINE
+my_socket.send(LINE + '\r\n')
+data = my_socket.recv(1024)
+print 'Recibido -- ', data
+print "Terminando socket..."
+my_socket.close()
+print "Fin."
