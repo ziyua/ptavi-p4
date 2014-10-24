45ddfff30a8d66f0ba33ebc79fdd85a4949f0e81
ziyua <z.y.ma@qq.com>
2014-10-24 16:48:16 +0200

    add expires time

--- client.py
+++ client.py
@@ -9,12 +9,16 @@ import sys
 
 # Cliente UDP simple.
 
+if len(sys.argv) != 6:
+    sys.exit('Usage: client.py ip puerto register sip_address expires_value')
+
 # Dirección IP del servidor.
 SERVER = sys.argv[1]
 PORT = int(sys.argv[2])
 
 # Contenido que vamos a enviar
-LINE = sys.argv[3].upper() + " " + sys.argv[4] + " " + 'SIP/2.0\r\n\r\n'
+LINE = sys.argv[3].upper() + ' ' + sys.argv[4] + ' ' + 'SIP/2.0\r\n'
+EXPIRES = 'Expires:' + ' ' + sys.argv[5] + '\r\n'
 
 # Creamos el socket, lo configuramos y lo atamos a un servidor/puerto
 my_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
@@ -22,7 +26,7 @@ my_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 my_socket.connect((SERVER, PORT))
 
 print "Enviando: " + LINE
-my_socket.send(LINE + '\r\n')
+my_socket.send(LINE + EXPIRES + '\r\n')
 data = my_socket.recv(1024)
 
 print 'Recibido -- ', data
